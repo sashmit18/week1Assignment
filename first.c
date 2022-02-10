90eeabaa83a543eab8f1fdaa57e628cb3eabbdf3
sashmit18 <99415820+sashmit18@users.noreply.github.com>
2022-02-10 23:07:39 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("Enter two values ");
+    scanf("%d %d", &x,&y);
+    z = x+y;
+    printf("The value of z is:%d",z);
+    return 0;
+}
